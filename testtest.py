b3b752f175aa571c81fa4aaac5b73daf22606e5a
Fredrik Boulund <boulund@users.noreply.github.com>
2017-02-21 16:51:50 +0100

    incomplete coverage
    incomplete coverage

--- testtest.py
+++ testtest.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import pytest
 
-def return2():
-    return 2
+def return2(n):
+    if n:
+        return 2
+    else:
+        return 1
     
 def test_return2():
-    assert(return2() == 2)
+    assert(return2(1) == 2)
